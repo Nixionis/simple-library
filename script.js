f064e86bc8f58d93bec025f22adc82903f544cd9
Nixionis <den-kosarev@mail.ru>
2023-10-07 05:08:07 +0700

    Init: Added layout for library
    Added with css grid

---
+++ script.js
@@ -0,0 +1,17 @@
+const booksArray = [];
+
+function Book(title, description, currentPage, maxPages, readStatus) {
+    this.title = title;
+    this.description = description;
+    this.currentPage = currentPage;
+    this.maxPages = maxPages;
+    this.readStatus = readStatus;
+}
+
+function addBook() {
+
+}
+
+document.querySelector('.header-button').addEventListener('click', (event) => {
+
+});
